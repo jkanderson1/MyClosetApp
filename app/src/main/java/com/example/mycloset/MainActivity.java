9c5a28a8074b1d206ca883de6f0bd177f33b5563
cdesmang <desmangc@email.sc.edu>
2023-10-25 18:58:28 -0400

    Added Button

--- app/src/main/java/com/example/mycloset/MainActivity.java
+++ app/src/main/java/com/example/mycloset/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mycloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Button Functionality
+        Button hello = findViewById(R.id.button);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
