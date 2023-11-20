d569eeb20018ab6db07d7222ab511decd0e12c8d
Dominic Campbell <dcamp11381@gmail.com>
2023-11-20 15:37:26 -0500

    updates to firebase

--- app/src/main/java/com/example/mycloset/HomeScreen.java
+++ app/src/main/java/com/example/mycloset/HomeScreen.java
@@ -3,12 +3,15 @@ package com.example.mycloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        button = findViewById(R.id.logout);
     }
 }
