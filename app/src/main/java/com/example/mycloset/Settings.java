d999800da22f56cc1127822b15c3f1d428e98f76
jkanderson1 <jordynka@email.sc.edu>
2023-11-20 17:35:36 -0500

    adding settings page and xml

--- app/src/main/java/com/example/mycloset/Settings.java
+++ app/src/main/java/com/example/mycloset/Settings.java
@@ -3,15 +3,12 @@ package com.example.mycloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
-    Button button;
+public class Settings extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-
-        button = findViewById(R.id.LogoutButton);
+        setContentView(R.layout.settings);
     }
 }
