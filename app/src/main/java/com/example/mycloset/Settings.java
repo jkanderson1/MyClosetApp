e6c36d3ff4ab8c7ac168aa4fb0d61ecd5cab4346
jkanderson1 <jordynka@email.sc.edu>
2023-11-20 18:07:50 -0500

    Settings button functionalities

--- app/src/main/java/com/example/mycloset/Settings.java
+++ app/src/main/java/com/example/mycloset/Settings.java
@@ -2,13 +2,31 @@ package com.example.mycloset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
 
+    Button account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        account.findViewById(R.id.AccountButton);
+
+        account.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
 }
