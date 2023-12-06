ff3533ded8d576e2f0c63a0f61e2eacd649007dc
sahiba-seth <sseth@email.sc.edu>
2023-12-06 10:00:40 -0500

    commit inventory xml

--- app/src/main/java/com/example/myclosetapp/Settings.java
+++ app/src/main/java/com/example/myclosetapp/Settings.java
@@ -3,13 +3,29 @@ package com.example.myclosetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class Settings extends AppCompatActivity {
 
+    public Button Preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Preferences = findViewById(R.id.PrefButton);
+        Preferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Preferences.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        }
+
     }
-}
