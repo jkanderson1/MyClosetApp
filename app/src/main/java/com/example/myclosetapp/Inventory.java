ed987e7e8b5036ae0ba8a260b7c347f3242a1be2
sahiba-seth <sseth@email.sc.edu>
2023-12-06 09:50:28 -0500

    commit inventory xml

--- app/src/main/java/com/example/myclosetapp/Inventory.java
+++ app/src/main/java/com/example/myclosetapp/Inventory.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Inventory extends AppCompatActivity {
-    public Button settings, addItem;
+
+    public Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +28,6 @@ public class Inventory extends AppCompatActivity {
                 finish();
             }
 
-        });
-        addItem = (Button) findViewById(R.id.addItemTV);
-        addItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AddItem.class);
-                startActivity(intent);
-                finish();
-            }
         });
 
 
