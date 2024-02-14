b19daa7d91ced20f108ca5e6972302671738282e
jkanderson1 <jordynka@email.sc.edu>
2024-02-13 19:45:05 -0500

    changes to inventory class

--- app/src/main/java/com/example/myclosetapp/Inventory.java
+++ app/src/main/java/com/example/myclosetapp/Inventory.java
@@ -2,6 +2,7 @@ package com.example.myclosetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,9 @@ public class Inventory extends AppCompatActivity {
     public Button settings;
     private Button addItem;
 
+    Button Home;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +46,15 @@ public class Inventory extends AppCompatActivity {
         }); {
 
         }
+        Home = (Button) findViewById(R.id.HomeScreen);
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
