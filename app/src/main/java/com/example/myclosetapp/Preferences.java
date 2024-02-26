893c170f31dd3dbee3b7f0b5bb9ad22714bf838d
sahiba-seth <sseth@email.sc.edu>
2024-02-25 21:03:18 -0500

    commit preferences

--- app/src/main/java/com/example/myclosetapp/Preferences.java
+++ app/src/main/java/com/example/myclosetapp/Preferences.java
@@ -1,14 +1,71 @@
 package com.example.myclosetapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class Preferences extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        mAuth = FirebaseAuth.getInstance();
+        save = findViewById(R.id.savePassButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePassword("currentPassword", "newPassword");
+                Intent intent = new Intent(getApplicationContext(), Settings.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+    private void changePassword(String currentPassword, String newPassword) {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            // Re-authenticate the user to make sure the current password is correct
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+            user.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // Update the password
+                                user.updatePassword(newPassword)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(Preferences.this, "Password Updated Sucessfully", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(Preferences.this, "Failed to update password", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            } else {
+                                Toast.makeText(Preferences.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
